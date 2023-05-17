96e92da33f6b3c3027b0b077bc5e4e99c32e7e90
cody-pierpoint <60990371+cody-pierpoint@users.noreply.github.com>
2023-05-17 15:57:05 +1000

    hangmanScript Attempt 2

---
+++ Assets/Scripts/HangManScriptAttempt2.cs
@@ -0,0 +1,168 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using TMPro;
+using UnityEngine;
+
+public class HangManScriptAttempt2 : MonoBehaviour
+{
+    //public struct HangSetUP
+    //{
+    //    public int letterIndex;
+    //    public TextMeshProUGUI[] textDisplay;
+    //}
+
+    //public HangSetUP baseSetup;
+
+    public bool letterFound;
+    public string[] wordList;
+    public string chosenWord;
+    public int correctLetters;
+    public int incorrectLetters;
+    public int incorrectLetterIndex;
+    public int correctLetterIndex;
+    public TextMeshProUGUI[] baseDisplay;
+    public TextMeshProUGUI[] guessedLetters;
+    public TextMeshProUGUI[] incorrectLetterDisplay;
+    public GameObject[] hangManDrawing;
+    public List<char> guessedCharacters = new List<char>(capacity: 26);
+    public bool gameOver;
+    private Event inputEvent;
+    public char inputCharacter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameSetup();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void GameSetup()
+    {
+        guessedCharacters.Clear();
+        for (int i = 0; i < hangManDrawing.Length; i++)
+        {
+            hangManDrawing[i].SetActive(false);
+
+        }
+        int randomWord = Random.Range(0, wordList.Length);
+        chosenWord = wordList[randomWord].ToUpper();
+        for (int i = 0; i < chosenWord.Length; i++)
+        {
+            baseDisplay[i].gameObject.SetActive(true);
+            guessedLetters[i].gameObject.SetActive(true);
+            guessedLetters[i].text = "";
+
+
+        }
+        correctLetters = chosenWord.Length;
+    }
+
+    public void OnGUI()
+    {
+        MakeGuess();
+    }
+
+    public void CorrectGuess()
+    {
+
+        for (int i = 0; i < chosenWord.Length; i++)
+        {
+            if (inputCharacter == chosenWord[i])
+            {
+                letterFound = true;
+                if (letterFound)
+                {
+
+                    guessedLetters[correctLetterIndex].text = inputCharacter.ToString().ToUpper();
+                    correctLetterIndex++;
+                }
+
+            }
+            
+            else
+            {
+                
+                letterFound = false;
+                Debug.LogWarning("CorrectGuess Lettfound set to false");
+            }
+        }
+        
+
+
+
+    }
+
+    public void IncorrectGuess()
+    {
+        for (int i = 0; i < chosenWord.Length; i++)
+        {
+            Debug.Log("incorrectGuess For loop");
+
+            if (inputCharacter != chosenWord[i])
+            {
+                letterFound = false;
+
+            }
+            if (!letterFound)
+            {
+                incorrectLetterDisplay[incorrectLetterIndex].text = inputCharacter.ToString().ToUpper();
+            }
+        }
+        
+
+
+    }
+
+    public void DrawHangMan()
+    {
+        for (int i = 0; i < hangManDrawing.Length; i++)
+        {
+            if (!letterFound)
+            {
+                hangManDrawing[incorrectLetterIndex].SetActive(true);
+            }
+        }
+    }
+
+    public void MakeGuess()
+    {
+
+        inputEvent = Event.current;
+
+
+        Debug.Log("eventType.keydown");
+        //
+        inputCharacter = char.ToUpper(inputEvent.character);
+        Debug.Log("input Character to upper");
+        if (inputCharacter >= 'A' && inputCharacter <= 'Z')
+        {
+            if (!guessedCharacters.Contains(inputCharacter))
+            {
+                guessedCharacters.Add(inputCharacter);
+
+                Debug.Log("add input to guessed character");
+                CorrectGuess();
+                if (!letterFound)
+                {
+                    Debug.LogWarning("Letter Always Found");
+                    IncorrectGuess();
+                    DrawHangMan();
+                    incorrectLetterIndex++;
+
+                }
+            }
+
+        }
+
+
+
+
+
+
+    }
+}
